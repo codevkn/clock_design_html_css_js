dbd30b58caf9a1d0ca0a556575885c36782aaad9
codevkn <karim.nesrine07@gmail.com>
2024-02-09 15:33:00 +0100

    script added

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+document.addEventListener('DOMContentLoaded', () =>
+  requestAnimationFrame(updateTime)
+)
+
+function updateTime() {
+  document.documentElement.style.setProperty('--timer-day', "'" + moment().format("dd") + "'");
+  document.documentElement.style.setProperty('--timer-hours', "'" + moment().format("k") + "'");
+  document.documentElement.style.setProperty('--timer-minutes', "'" + moment().format("mm") + "'");
+  document.documentElement.style.setProperty('--timer-seconds', "'" + moment().format("ss") + "'");
+  requestAnimationFrame(updateTime);
+}
